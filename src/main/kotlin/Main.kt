fa0e58a0f18b62ffc74b8009f404f7c676872e54
kaz <kazan19066@gmail.com>
2024-01-19 21:34:24 +0400

    fix errors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main() {
 
-    val post1 = WallService.add(" Доброй ночи! ")
+    val post1 = WallService.add(Post(text =" Доброй ночи! "))
     println(post1)
 
-    val post2 = WallService.add(" С праздником! ")
+    val post2 = WallService.add(Post(text = " С праздником! "))
     println(post2)
 
     val postToUpdate = Post(id = 1, text = " Не желаю доброй ночи")
@@ -28,35 +28,35 @@ data class Post(
     var isFavorite: Boolean = true,
     var isPinned: Boolean = false,
     var canEdit: Boolean = true,
+    var comments: Comments = Comments()
     )
 
-object comments {
-    val count: Int = 15
-    var canPost: Boolean = true
-    var groupsCanPost: Boolean = true
-    var canClose: Boolean = false
+data class Comments (
+    val count: Int = 15,
+    var canPost: Boolean = true,
+    var groupsCanPost: Boolean = true,
+    var canClose: Boolean = false,
     var canOpen: Boolean = true
-}
-
-data class postList(
-    val key: Int,
-    val value: Post
 )
 
 object WallService {
     private var uniqueId = 0
-    private var posts = ArrayList<postList>()
-    fun add(post: String): Post {
-        val newPost = Post(id = uniqueId++, text = post)
-        val postEntry = postList(key = newPost.id, value = newPost)
-        posts.add(postEntry)
+    private var posts = ArrayList<Post>()
+
+    fun clear() {
+        posts =  ArrayList<Post>()
+        uniqueId = 0
+    }
+    fun add(post: Post): Post {
+        val newPost = post.copy(id = ++uniqueId, comments = post.comments.copy())
+        posts.add(newPost)
         return newPost
     }
 
     fun update(post: Post): Boolean {
-        for (entry in posts) {
-            if (entry.key == post.id) {
-                entry.value.text = post.text
+        for (i in posts.indices) {
+            if (posts[i].id == post.id) {
+                posts[i] = post.copy(comments = post.comments.copy())
                 return true
             }
         }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,28 +1,34 @@
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class WallServiceTest {
-
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
     @Test
     fun add() {
         val wallService = WallService
-        val result = wallService.add("Тестирую функцию добавления")
+        val result = wallService.add(Post(text  = "Тестирую функцию добавления"))
         assertEquals(1, result.id)
     }
+
     @Test
     fun updateSuccessfully() {
         val wallService = WallService
-        val post1 = wallService.add("Тестирую функцию обновления")
+        val post1 = wallService.add(Post(text = "Тестирую функцию обновления"))
         val result = wallService.update(post1)
-        assertEquals(true, result)
+        assertTrue(result)
     }
     @Test
     fun updateUnsuccessfully() {
+
         val wallService = WallService
-        val post1 = wallService.add("Тестирую функцию обновления")
+        val post1 = wallService.add(Post(text= "Тестирую функцию обновления"))
         val postToUpdate = Post(id = 3, text = " Нет такого id")
         val result = WallService.update(postToUpdate)
-        assertEquals(false, result)
+        assertFalse(result)
     }
 }
